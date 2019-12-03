0e9b9e7ebf7487b73180a24540bb50a6c2152170
Daniel Adu-Djan <adudjan.danieladd@gmail.com>
2019-12-03 14:12:29 -0500

    Done

--- App-1/ViewController.swift
+++ App-1/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var initViewColor: UIColor!
+    var textColorButtonTapCount: Int = 0
+    var viewColorButtonTapCount: Int = 0
+    
+    @IBOutlet weak var introTextLabel: UILabel!
+    @IBOutlet weak var userInputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        initViewColor = view.backgroundColor
     }
 
-
+    @IBAction func changeIntroTextColor(_ sender: Any) {
+        if textColorButtonTapCount % 2 == 0 {
+            introTextLabel.textColor = UIColor.white
+        } else {
+            introTextLabel.textColor = UIColor.black
+        }
+        textColorButtonTapCount += 1
+    }
+    
+    @IBAction func changeViewColor(_ sender: Any) {
+        if viewColorButtonTapCount % 2 == 0 {
+            view.backgroundColor = UIColor.purple
+        } else {
+            view.backgroundColor = initViewColor
+        }
+        viewColorButtonTapCount += 1
+    }
+    
+    @IBAction func changeText(_ sender: Any) {
+        //introTextLabel.text = "Goodbye!"
+        if userInputTextField.text != "" {
+            introTextLabel.text = userInputTextField.text
+            userInputTextField.text = ""
+            view.endEditing(true)
+        }
+    }
+    
+    @IBAction func resetToInitialView(_ sender: Any) {
+        introTextLabel.text = "Hello from Daniel!"
+        introTextLabel.textColor = UIColor.black
+        view.backgroundColor = initViewColor
+        textColorButtonTapCount = 0
+        viewColorButtonTapCount = 0
+    }
 }
 
